fdb4cf086edee0424881fe28b54a7991868e642d
Basile Monier <basilemonier@gmail.com>
2026-04-10 16:24:56 +0200

    feat: add game in one round

--- match.py
+++ match.py
@@ -13,3 +13,20 @@ def choice(person) :
     if person["human"] == False :
         ia_choice = random.choice (["Pierre", "Feuille", "Ciseaux"])
         return ia_choice
+    
+
+def game_1 (p1, p2) :
+    p1_play = choice(p1)
+    p2_play = choice(p2)
+    choice_win = {"Pierre": "Ciseaux", "Feuille": "Pierre", "Ciseaux": "Feuille"}
+    if choice_win[p1_play] == p2_play :
+        print (f"{p1} à gagner cette manche !")
+        return p1
+    if choice_win[p1_play] != p2_play :
+        print (f"{p2} à gagner cette manche !")
+        return p2
+    if p1_play == p2_play :
+        print ("Égalité, vous avez fais pareil.")
+        return None
+
+
